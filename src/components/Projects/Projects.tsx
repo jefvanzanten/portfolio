648b47df804d6982ae31878dc601c3edc26c58df
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-28 14:32:23 +0200

    Projecten aanpassing test

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,19 +1,21 @@
-import "./Projects.css";
+import styles from "./Projects.module.css";
 
 export default function Projects() {
   return (
     <main>
-      <div className="projects-container">
-        <div className="project-list-container">
-          <ul className="project-list">
-            <li className="active-project">Full Stack Todo</li>
+      <div className={styles["container"]}>
+        <div className={styles["project-list-container"]}>
+          <ul className={styles["project-list"]}>
+            <li className={styles["active-project"]}>
+              Fullstack Todo + Mobile app
+            </li>
             <li>Reeksenspel webapp</li>
-            <li>School project: OV app Full Stack</li>
+            <li>School project: OV app Fullstack</li>
             <li>SupplementenManager voor Android</li>
           </ul>
         </div>
-        <div className="project-details">
-          <p className="project-details-text">Uitleg</p>
+        <div className={styles["project-details"]}>
+          <p className={styles["project-details-text"]}>Uitleg</p>
         </div>
       </div>
     </main>
