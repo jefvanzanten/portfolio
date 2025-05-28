fab847237897b341d39e8e47981466c6ba2d1676
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-28 13:18:00 +0200

    removed github from contact page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,10 +5,6 @@ export default function Contact() {
   return (
     <main className={styles.content}>
       <SocialLink
-        iconUrl={"github-mark-white.svg"}
-        url={"https://github.com/JeffreyvnZanten"}
-      />
-      <SocialLink
         iconUrl={"LI-Logo.png"}
         url={"https://www.linkedin.com/in/jefvanzanten"}
       />
