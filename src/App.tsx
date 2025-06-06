2b8c7291994dc7c5b4d7f4d7b88974e2663150e6
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-06-06 12:28:29 +0200

    didnt save this

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navigation from "./components/navigation_change/Navigation";
+import Navigation from "./components/Navigation/Navigation";
 import Router from "./components/Router";
 
 function App() {
