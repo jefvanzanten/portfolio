2713437ccd6098bea6fc26814dab9832cc68da49
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-06-16 13:40:27 +0200

    New layout also for desktop and tablet

---
+++ src/components/ProjectCategoryCard/ProjectCategoryCard.tsx
@@ -0,0 +1,30 @@
+import styles from "./ProjectCategoryCard.module.css";
+import { Link } from "react-router";
+import { Project } from "../Projects/Projects";
+
+type ProjectCategoryCardProps = {
+  projects: Project[];
+};
+
+export default function ProjectCategoryCard({
+  projects,
+}: ProjectCategoryCardProps) {
+  return (
+    <Link to={`/projects/${projects[0].category.toLowerCase()}`}>
+      <div className={styles["project-category-card"]}>
+        <img
+          src="/frontend_icon_3.svg"
+          alt="Frontend Icon"
+          className={styles["category-icon"]}
+        />
+        <div className={styles["project-info-container"]}>
+          <div className={styles["project-info"]}>
+            <h2 className={styles.h2}>{projects[0].category}</h2>
+            <p className={styles.p}>{projects.length} projecten</p>
+          </div>
+          <img src="/open_icon.svg" className={styles["next-icon"]} />
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,6 +4,7 @@ import { getRecentProjects } from "../../helper";
 import { Link } from "react-router";
 import ProjectCarousel from "../ProjectCarousel/ProjectCarousel";
 import useProjects from "../../hooks/useProjects";
+import ProjectCategoryCard from "../ProjectCategoryCard/ProjectCategoryCard";
 
 export type Project = {
   id: number;
@@ -42,74 +43,10 @@ export default function Projects() {
       <section className={styles.section}>
         <h1 className={styles.h1}>Categoriën</h1>
         <div className={styles["project-list"]}>
-          <Link to={"/projects/frontend"}>
-            <div className={styles["project-card"]}>
-              <img
-                src="/frontend_icon_3.svg"
-                alt="Frontend Icon"
-                className={styles.icon}
-              />
-              <div className={styles["project-info-container"]}>
-                <div className={styles["project-info"]}>
-                  <h2 className={styles.h2}>Frontend</h2>
-                  <p className={styles.p}>
-                    {frontendProjects.length} projecten
-                  </p>
-                </div>
-                <img src="/open_icon.svg" className={styles["icon-open"]} />
-              </div>
-            </div>
-          </Link>
-          <Link to={"/projects/backend"}>
-            <div className={styles["project-card"]}>
-              <img
-                src="/frontend_icon_3.svg"
-                alt="Frontend Icon"
-                className={styles.icon}
-              />
-              <div className={styles["project-info-container"]}>
-                <div className={styles["project-info"]}>
-                  <h2 className={styles.h2}>Backend</h2>
-                  <p className={styles.p}>{backendProjects.length} projecten</p>
-                </div>
-                <img src="/open_icon.svg" className={styles["icon-open"]} />
-              </div>
-            </div>
-          </Link>
-          <Link to={"/projects/fullstack"}>
-            <div className={styles["project-card"]}>
-              <img
-                src="/frontend_icon_3.svg"
-                alt="Frontend Icon"
-                className={styles.icon}
-              />
-              <div className={styles["project-info-container"]}>
-                <div className={styles["project-info"]}>
-                  <h2 className={styles.h2}>Fullstack</h2>
-                  <p className={styles.p}>
-                    {fullStackProjects.length} projecten
-                  </p>
-                </div>
-                <img src="/open_icon.svg" className={styles["icon-open"]} />
-              </div>
-            </div>
-          </Link>
-          <Link to={"/projects/mobile"}>
-            <div className={styles["project-card"]}>
-              <img
-                src="/frontend_icon_3.svg"
-                alt="Frontend Icon"
-                className={styles.icon}
-              />
-              <div className={styles["project-info-container"]}>
-                <div className={styles["project-info"]}>
-                  <h2 className={styles.h2}>Mobile</h2>
-                  <p className={styles.p}>{mobileProjects.length} projecten</p>
-                </div>
-                <img src="/open_icon.svg" className={styles["icon-open"]} />
-              </div>
-            </div>
-          </Link>
+          <ProjectCategoryCard projects={frontendProjects} />
+          <ProjectCategoryCard projects={backendProjects} />
+          <ProjectCategoryCard projects={fullStackProjects} />
+          <ProjectCategoryCard projects={mobileProjects} />
         </div>
       </section>
     </main>
