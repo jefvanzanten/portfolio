331abee7f5ee85fa73b4b26df0c6c6d3c86d67c3
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-06-16 12:22:03 +0200

    Mobile only new layout for projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -44,26 +44,70 @@ export default function Projects() {
         <div className={styles["project-list"]}>
           <Link to={"/projects/frontend"}>
             <div className={styles["project-card"]}>
-              <h2 className={styles.h2}>Frontend</h2>
-              <p className={styles.p}>{frontendProjects.length} projecten</p>
+              <img
+                src="/frontend_icon_3.svg"
+                alt="Frontend Icon"
+                className={styles.icon}
+              />
+              <div className={styles["project-info-container"]}>
+                <div className={styles["project-info"]}>
+                  <h2 className={styles.h2}>Frontend</h2>
+                  <p className={styles.p}>
+                    {frontendProjects.length} projecten
+                  </p>
+                </div>
+                <img src="/open_icon.svg" className={styles["icon-open"]} />
+              </div>
             </div>
           </Link>
           <Link to={"/projects/backend"}>
             <div className={styles["project-card"]}>
-              <h2 className={styles.h2}>Backend</h2>
-              <p className={styles.p}>{backendProjects.length} projecten</p>
+              <img
+                src="/frontend_icon_3.svg"
+                alt="Frontend Icon"
+                className={styles.icon}
+              />
+              <div className={styles["project-info-container"]}>
+                <div className={styles["project-info"]}>
+                  <h2 className={styles.h2}>Backend</h2>
+                  <p className={styles.p}>{backendProjects.length} projecten</p>
+                </div>
+                <img src="/open_icon.svg" className={styles["icon-open"]} />
+              </div>
             </div>
           </Link>
           <Link to={"/projects/fullstack"}>
             <div className={styles["project-card"]}>
-              <h2 className={styles.h2}>Fullstack</h2>
-              <p className={styles.p}>{fullStackProjects.length} projecten</p>
+              <img
+                src="/frontend_icon_3.svg"
+                alt="Frontend Icon"
+                className={styles.icon}
+              />
+              <div className={styles["project-info-container"]}>
+                <div className={styles["project-info"]}>
+                  <h2 className={styles.h2}>Fullstack</h2>
+                  <p className={styles.p}>
+                    {fullStackProjects.length} projecten
+                  </p>
+                </div>
+                <img src="/open_icon.svg" className={styles["icon-open"]} />
+              </div>
             </div>
           </Link>
           <Link to={"/projects/mobile"}>
             <div className={styles["project-card"]}>
-              <h2 className={styles.h2}>Mobile</h2>
-              <p className={styles.p}>{mobileProjects.length} projecten</p>
+              <img
+                src="/frontend_icon_3.svg"
+                alt="Frontend Icon"
+                className={styles.icon}
+              />
+              <div className={styles["project-info-container"]}>
+                <div className={styles["project-info"]}>
+                  <h2 className={styles.h2}>Mobile</h2>
+                  <p className={styles.p}>{mobileProjects.length} projecten</p>
+                </div>
+                <img src="/open_icon.svg" className={styles["icon-open"]} />
+              </div>
             </div>
           </Link>
         </div>
