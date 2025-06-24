96d5e0a1fd084f9b6df478c7cfa2fd4b638a4cb0
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-06-24 12:51:59 +0200

    Social links now open in a new tab or window

--- src/components/SocialLink/SocialLink.tsx
+++ src/components/SocialLink/SocialLink.tsx
@@ -8,7 +8,7 @@ type SocialLinkProps = {
 
 export default function SocialLink({ name, url, iconUrl }: SocialLinkProps) {
   return (
-    <a className={styles.socialContainer} href={url}>
+    <a className={styles.socialContainer} href={url} target="_blank">
       <img className={styles.logo} src={iconUrl} />
       <p className={styles.p}>{name}</p>
     </a>
