d7f75e0c096b83b22fe59100e0454d09f21840b7
Jef van Zanten <j.van.zanten@gmail.com>
2025-08-24 20:50:19 +0200

    Adjusted projects.json and removed the old projects code.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,14 @@
 import { Route, Routes } from "react-router";
-import Projects from "./components/Projects/Projects";
 // import About from "./About/About";
 import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import ProjectsCategory from "./components/ProjectsCategory/ProjectsCategory";
 import ProjectPage from "./components/ProjectPage/ProjectPage";
-import NewProjects from "./components/Projects/NewProject";
+import Projects from "./components/Projects/Projects";
 
 export const navItems = [
   { to: "/", name: "Home", element: <Home /> },
-  { to: "/projects", name: "Projecten", element: <NewProjects /> },
+  { to: "/projects", name: "Projecten", element: <Projects /> },
   // { to: "/about", name: "Over mij", element: <About /> },
   { to: "/contact", name: "Contact", element: <Contact /> },
 ];
