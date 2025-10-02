ae2cfc0030a1b26f169ded31ebe0c065dad771dd
Jef van Zanten <j.van.zanten@gmail.com>
2025-10-02 15:04:04 +0200

    Changed the Home/Landing page so it contains my featured projects.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,8 @@
 import { Link, NavLink } from "react-router";
 import styles from "./Home.module.css";
 import NavButton from "../NavButton/NavButton";
+import useProjects from "../../hooks/useProjects";
+import ProjectCard from "../ProjectCard/ProjectCard";
 
 const data = [
   { name: "React", url: "https://react.dev/" },
@@ -20,26 +22,50 @@ const data = [
 ];
 
 export default function Home() {
+  const { loading, projects } = useProjects();
+
+  const filteredProjects = projects.filter((project) => project.highlighted);
+
   return (
     <main>
-      <div className={styles["home-container"]}>
-        <h1 className={styles.h1}>
-          Ik maak fullstack web en mobiele applicaties
-        </h1>
-        <p className={styles.p}>
-          Hey, mijn naam is Jef. Momenteel volg ik de studie Software
-          Development (HBO Associate Degree).
-          <br />
-          <br />
-          Ik wissel met programmeren tussen frontend, backend en mobile
-          projecten, zodat mijn kennis en vaardigheden op elk vlak blijven
-          ontwikkelen.
-        </p>
-        <div className={styles["button-container"]}>
-          <NavButton title="Bekijk projecten" to="/projects" />
-          <NavButton title="Neem contact" to="/contact" />
-        </div>
+      <div className={styles["content-container"]}>
+        <section className={styles.introduction}>
+          <h1 className={styles.h1}>Full-stack en mobile developer</h1>
+
+          <p className={styles.p}>
+            Hey, mijn naam is Jef. Momenteel volg ik de studie Software
+            Development (HBO Associate Degree).
+            <br />
+            <br />
+            Ik wissel met programmeren tussen frontend, backend en mobile
+            projecten, zodat mijn kennis en vaardigheden op elk vlak blijven
+            ontwikkelen.
+          </p>
+          <div className={styles["button-container"]}>
+            <NavButton title="Bekijk projecten" to="/projects" />
+            <NavButton title="Neem contact" to="/contact" />
+          </div>
+        </section>
+
+        <section className={styles["featured-projects-container"]}>
+          <div className={styles.title}>
+            <span className={styles.line} />
+            <h1>Featured Projects</h1>
+            <span className={styles.line} />
+          </div>
+
+          <div className={styles["featured-projects-list"]}>
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              filteredProjects.map((project) => (
+                <ProjectCard key={project.id} project={project} />
+              ))
+            )}
+          </div>
+        </section>
       </div>
+
       <footer className={styles.footer}>
         {data.map((item, index) => (
           <a
